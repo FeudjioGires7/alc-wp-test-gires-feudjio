2bec929c962d255289c2a09d2c47aeda06f5a4ae
gires <giresjaphet@gmail.com>
2025-08-04 16:03:30 +0100

    cgange

--- testimonials.js
+++ testimonials.js
@@ -5,23 +5,23 @@ const testimonials = [
     },
     {
         titre: "Orange Cameroun",
-        contenu: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        contenu: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolorem expedita deleniti necessitatibus pariatur culpa unde dolor vero reprehenderit odio.",
     },
     {
         titre: "Mtn Cameroun",
-        contenu: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        contenu: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia totam magnam, ratione doloribus voluptatum reprehenderit ipsum libero voluptate, vel laborum assumenda fugit unde odit? Harum animi corporis minus enim!",
     },
     {
         titre: "SpaceX",
-        contenu: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        contenu: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus sit, quasi explicabo repudiandae iste, reiciendis consequuntur, totam praesentium ut distinctio magnam ratione! Tempora!",
     },
     {
         titre: "Facebook",
-        contenu: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        contenu: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, atque voluptatem. Doloribus expedita enim id. Ex iusto consectetur ratione nisi ipsa sequi animi tenetur officiis?",
     },
     {
         titre: "DropBox",
-        contenu: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        contenu: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fuga necessitatibus, corporis placeat ipsa optio recusandae itaque, perferendis libero impedit hic aliquam.",
     },
 ]
 
