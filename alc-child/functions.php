11354b070c5dc34f4c714244cee125c1a153dd50
gires <giresjaphet@gmail.com>
2025-08-04 12:35:09 +0100

    change

--- alc-child/functions.php
+++ alc-child/functions.php
@@ -1,4 +1,28 @@
-<?php
-// Your code to enqueue parent theme styles
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Alc</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <header class="bg-green-400 opacity-75">
+        <nav class="flex justify-between items-center p-4">
+            <p>
+                Hero <span class="text-yellow-700">ALC</span>
+            </p>
+            <ul class="flex space-x-4 flex-row">
+                <li>List des articles</li>
+                <li>A Propos</li>
+                <li>Commentaires</li>
+            </ul>
+        </nav>
+    </header>
+    <div class="border-2 border-radius-2 border-red-500 p-4 w-[70%] mx-auto mt-4 rounded-xl">
+        <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et in fugit fuga, at veritatis modi dolor nemo veniam alias minus sequi eligendi eveniet perferendis. Enim eum debitis aliquid molestias ad.</p>
+        <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et in fugit fuga, at veritatis modi dolor nemo veniam alias minus sequi eligendi eveniet perferendis. Enim eum debitis aliquid molestias ad.</p>
+        <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et in fugit fuga, at veritatis modi dolor nemo veniam alias minus sequi eligendi eveniet perferendis. Enim eum debitis aliquid molestias ad.</p>
+    </div>
+</body>
+</html>
